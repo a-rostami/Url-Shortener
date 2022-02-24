efe927c6dc90b048671f42dbf0dddb301aa1e0ae
Arash Rostami <a.rostami2400@gmail.com>
2022-02-24 20:34:29 +0330

    initialization commit.

---
+++ src/test/java/com/rostami/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rostami.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
